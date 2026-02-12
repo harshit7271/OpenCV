e0273e22f2aa38c6e42bb5fdc00e9fe3d7899003
Harshit Singh <harshitsingh05893312@gmail.com>
2026-02-13 01:24:54 +0530

    Updated

--- load_img.py
+++ load_img.py
@@ -3,9 +3,9 @@ import cv2
 # To read the image
 flag = cv2.IMREAD_COLOR
 
-image = ("colouredTurtle copy.png", flag)
+image = cv2.imread("colouredTurtle copy.png", flag)
 
 if image is None:
     print("Image not found or failed to load")
 else:
-    print("Image loaded successfully:", image)
+    print("Image loaded successfully:", image.shape)
