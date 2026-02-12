4e225adc81cfa67742770a30f1392b04a30f1077
Harshit Singh <harshitsingh05893312@gmail.com>
2026-02-13 01:41:42 +0530

    Updated

--- gray_scale_conversion.py
+++ gray_scale_conversion.py
@@ -5,10 +5,13 @@ import cv2
 image = cv2.imread("colouredTurtle copy.png")
 
 if image is not None:
+    # conversion
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # display
     cv2.imshow("Grayscale Image", gray)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    # save
     cv2.imwrite("gray_scale.png", gray)
 else:
     print("Unable to load")
