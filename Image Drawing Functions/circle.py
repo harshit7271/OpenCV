6a1a4952f414622d54a149d8a4ecd434fd55d1ad
Harshit Singh <harshitsingh05893312@gmail.com>
2026-02-14 03:26:14 +0530

    Update

---
+++ Image Drawing Functions/circle.py
@@ -0,0 +1,17 @@
+import cv2
+
+image = cv2.imread("colouredTurtle copy.png")
+
+if image is None:
+    print("Image not loaded")
+else:
+    print("Loaded")
+
+    cv2.circle(image, (250, 170), 150, (0, 255, 0), thickness=-1)
+
+    cv2.imshow("Circle", image)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    print(image.shape)
