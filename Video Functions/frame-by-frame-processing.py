71c13a6a7b6df9d2af4eb8c1f036d6e3141f940f
Harshit Singh <harshitsingh05893312@gmail.com>
2026-02-17 19:35:34 +0530

    Updated

--- Video Functions/frame-by-frame-processing.py
+++ Video Functions/frame-by-frame-processing.py
@@ -6,3 +6,25 @@ camera = cv2.VideoCapture(0)
 
 frame_width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+codec = cv2.VideoWriter_fourcc(*'XVID')  # type: ignore
+recorded = cv2.VideoWriter("my_video.avi", codec, 20,
+                           (frame_width, frame_height))
+
+while True:
+    success, frame = camera.read()
+
+    if not success:
+        print("Could not read the frame")
+        break
+
+    recorded.write(frame)
+    cv2.imshow('Recording live', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        print("Quitting....")
+        break
+
+camera.release()
+recorded.release()
+cv2.destroyAllWindows()
