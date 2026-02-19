791d09f12c1bfb251d29b3bd9fc05d2d387c4c37
Harshit Singh <harshitsingh05893312@gmail.com>
2026-02-20 03:09:42 +0530

    Update

--- Contour & Shape Detction/contour_fun.py
+++ Contour & Shape Detction/contour_fun.py
@@ -14,6 +14,31 @@ contours, heirarchy = cv2.findContours(
 # DRAW CONTOURS :  cv2.drawContours(image, contours, contourIdx, color, thickness)
 img = cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
 
+for contour in contours:
+    # it will give us the corner points of the contour
+    approx = cv2.approxPolyDP(contour, 0.01*cv2.arcLength(contour, True), True)
+
+    corners = len(approx)
+
+    if corners == 3:
+        shape_name = "Triangle"
+    elif corners == 4:
+        shape_name = "Rectangle"
+    elif corners == 5:
+        shape_name = "Pentagon"
+    elif corners > 5:
+        shape_name = "Circle"
+    else:
+        shape_name = "Unknown"
+
+    cv2.drawContours(img, [approx], 0, (0, 255, 0), 2)
+    # it will give us the x coordinate of the first corner point AND RAVEL will convert the 2D array to 1D array
+    x = approx.ravel()[0]
+    # it will give us the y coordinate of the first corner point and we are subtracting 10 to move the text above the contour
+    y = approx.ravel()[1] - 10
+    cv2.putText(img, shape_name, (x, y),
+                cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 0, 0), 1)
+
 cv2.imshow("Contours", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
